5a417df848d30302946cdd1a4f9d42dc8e1b1c91
mass10 <mass10.github@gmail.com>
2016-12-31 15:54:14 +0900

    wip

--- app/index.js
+++ app/index.js
@@ -1,38 +1,36 @@
 #!/usr/bin/env node
 // coding: utf-8
 
+var __sessions = 0;
+
 function _startup() {
 
 }
 
-function handler_template_dashboard(req, res, next) {
+function _handler_template_dashboard(req, res, next) {
 
 	var content = {name: "中臣鎌足"};
 	res.render("dashboard.ejs", content);
 }
 
-function handler_template_preferences(req, res, next) {
+function _handler_template_preferences(req, res, next) {
 
 	var content = {name: "中臣鎌足"};
 	res.render("preferences.ejs", content);
 }
 
-function handler_simple_root(req, res, next) {
+function _handler_simple_root(req, res, next) {
 
 	res.redirect("/dashboard");
 }
 
-function handler_simple_json(req, res, next) {
+function _handler_simple_json(req, res, next) {
 
-	//
-	// json
-	//
 	var content = {
 		status: "OK",
 		message_text: "Hello",
 		timestamp: new Date()
 	};
-
 	res.json(content);
 }
 
@@ -44,7 +42,7 @@ function _load_json_file(path) {
 	return "" + fs.readFileSync(path, "utf8");
 }
 
-function handler_load_boxex(req, res, next) {
+function _handler_load_boxex(req, res, next) {
 
 	try {
 		var json = _load_json_file("/tmp/.peta2-boxes-data.json");
@@ -52,30 +50,75 @@ function handler_load_boxex(req, res, next) {
 	}
 	catch (err) {
 		console.log(err);
-		console.log("[ERROR] json データの読み込みに失敗しています。");
+		console.log("[error] json データの読み込みに失敗しています。");
 		res.json({});
 	}
 }
 
+function _read_arguments() {
+
+	const args = require('command-line-args');
+	const def = [
+		{name: 'help', alias: 'h', type: Boolean},
+		{name: 'host', type: String, defaultValue: "127.0.0.1"},
+		{name: 'port', alias: 'p', type: Number, defaultValue: 80}
+	];
+	return args(def);
+}
+
+function _usage() {
+
+	console.log("USAGE:");
+	console.log("    --help: show this message.");
+	console.log("    --host: interface to listen.");
+	console.log("    --port: port to listen.");
+	console.log("");
+}
+
 function main() {
 
+	// ========================================================================
+	// コマンドラインオプションの取り出し
+	// ========================================================================
+	const options = _read_arguments();
+	if (options['help']) {
+		_usage();
+		return;
+	}
+
+	console.log("[trace] runnung on [" + __dirname + "]");
+	console.log("[trace] running with options " + JSON.stringify(options));
+
+	// ========================================================================
+	// アプリケーションの初期化
+	// ========================================================================
 	var express = require("express");
-	var ejs_template_engine = require("ejs")
+	require("ejs")
 	var app = express();
 	app.set('view engine', 'ejs');
 	var http = require('http').Server(app);
 	var io = require('socket.io')(http);
 
-	app.get("/", handler_simple_root);
-	app.get("/hello", handler_simple_json);
-	app.get("/dashboard", handler_template_dashboard);
-	app.get("/preferences", handler_template_preferences);
-	app.get("/boxes", handler_load_boxex);
+	// ========================================================================
+	// ルーティング設定
+	// ========================================================================
+	app.get("/", _handler_simple_root);
+	app.get("/hello", _handler_simple_json);
+	app.get("/dashboard", _handler_template_dashboard);
+	app.get("/preferences", _handler_template_preferences);
+	app.get("/boxes", _handler_load_boxex);
 
+	// ========================================================================
+	// Socket.IO のイベント設定
+	// ========================================================================
 	io.on('connection', function(socket) {
 		console.log('[TRACE] Welcome! << (ﾟ _ﾟ )))');
+		__sessions++;
+		io.emit('sessions changed', {count: __sessions});
 		socket.on('disconnect', function() {
 			console.log('[TRACE] Bye... >> ((( ﾟ_ ﾟ)');
+			__sessions--;
+			io.emit('sessions changed', {count: __sessions});
 		});
 		socket.on('chat message', function(m) {
 			console.log('[TRACE] caught message: {x:' + m.x +", y:" + m.y + ", text:" + m.text + "} << (PEER)");
@@ -84,7 +127,10 @@ function main() {
 		});
 	});
 
-	var server = http.listen(8080, _startup);
+	// ========================================================================
+	// サーバーを起動
+	// ========================================================================
+	var server = http.listen(options["port"], _startup);
 	console.log("Node.js is listening to port: " + server.address().port);
 }
 
