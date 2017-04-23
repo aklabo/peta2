837d3b3ae459f700e8104790dc75f84f1e31f81a
mass10 <mass10.github@gmail.com>
2017-04-23 07:42:15 +0000

    wip

--- app/index.js
+++ app/index.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 // coding: utf-8
 
+
+
 ///////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////
@@ -28,8 +30,127 @@ var util = {
 		var g = 200 + Math.floor(Math.random() * 56);
 		var b = 200 + Math.floor(Math.random() * 56);
 		return util.rgb(r, g, b);
+	},
+
+	get_timestamp: function() {
+		var date = new Date();
+		var month = date.getMonth() + 1;
+		var day = date.getDate();
+		var hour = date.getHours();
+		var min = date.getMinutes();
+		var sec = date.getSeconds();
+		var millisec = date.getMilliseconds();
+		month = (month < 10 ? "0" : "") + month;
+		day = (day < 10 ? "0" : "") + day;
+		hour = (hour < 10 ? "0" : "") + hour;
+		min = (min < 10 ? "0" : "") + min;
+		sec = (sec < 10 ? "0" : "") + sec;
+		millisec = (millisec < 100 ? "00" : "") + millisec;
+		return "" + date.getFullYear() + "-" + month + "-" + day + " " +  hour + ":" + min + ":" + sec + "." + millisec;
 	}
 };
+
+
+
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+var logger = {
+
+	info: function(message) {
+		try {
+			require("json");
+			var fs = require("fs");
+			fs.appendFileSync("peta2.log", util.get_timestamp() + " [INFO] " + message + "\n", {encoding: "utf-8"});
+		}
+		catch (ex) {
+			console.log(ex);
+			console.log("[ERROR] ログ出力に失敗しています。");
+		}
+	},
+
+	trace: function(message) {
+		try {
+			require("json");
+			var fs = require("fs");
+			fs.appendFileSync("peta2.log", util.get_timestamp() + " [TRACE] " + message + "\n", {encoding: "utf-8"});
+		}
+		catch (ex) {
+			console.log(ex);
+			console.log("[ERROR] ログ出力に失敗しています。");
+		}
+	},
+
+	error: function(message) {
+		try {
+			require("json");
+			var fs = require("fs");
+			fs.appendFileSync("peta2.log", util.get_timestamp() + " [ERROR] " + message + "\n", {encoding: "utf-8"});
+		}
+		catch (ex) {
+			console.log(ex);
+			console.log("[ERROR] ログ出力に失敗しています。");
+		}
+	}
+};
+
+
+
+
+
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+
+function _load_json_file(path) {
+
+	try {
+		require("json");
+		var fs = require("fs");
+		return JSON.parse(fs.readFileSync(path, "utf-8"));
+	}
+	catch (ex) {
+		logger.info(ex);
+		logger.info("[info] ファイルが存在しません。");
+		return {}
+	}
+}
+
+function _save_box(m) {
+
+	try {
+		var tree_object = _load_json_file(".peta2-boxes-data.json");
+		require("json");
+		if (tree_object["data"] == null) {
+			tree_object["data"] = [];
+		}
+		boxes_array = tree_object["data"];
+		boxes_array.push(m);
+		var fs = require("fs");
+		var json_data = JSON.stringify(tree_object, null, "\t");
+		fs.writeFileSync(".peta2-boxes-data.json", json_data, {encoding: "utf-8"});
+	}
+	catch (err) {
+		logger.trace(err);
+		logger.error("json データの保存に失敗しています。");
+	}
+}
 
 function application() {
 
@@ -39,9 +160,9 @@ function application() {
 
 	this._on_new_peer = function(socket) {
 
-		_log("[TRACE] Welcome! << (ﾟ _ﾟ )))");
+		logger.trace("Welcome! << (ﾟ _ﾟ )))");
 		this._sessions++;
-		_log("(" + this._sessions + ") connection(s) alive.");
+		logger.info("(" + this._sessions + ") connection(s) alive.");
 		this._io.emit("sessions changed", {count: this._sessions});
 		var peer = new peer_connection(this, this._io, socket);
 	}.bind(this);
@@ -76,12 +197,12 @@ function application() {
 	this._handler_load_boxex = function(req, res, next) {
 
 		try {
-			var json = _load_json_file("/tmp/.peta2-boxes-data.json");
+			var json = _load_json_file(".peta2-boxes-data.json");
 			res.json(json);
 		}
 		catch (err) {
-			_log(err);
-			_log("[error] json データの読み込みに失敗しています。");
+			logger.trace(err);
+			logger.error("json データの読み込みに失敗しています。");
 			res.json({});
 		}
 	}.bind(this);
@@ -92,9 +213,9 @@ function application() {
 
 	this.run = function(options) {
 
-		_log("[trace] ### START ###");
-		_log("[trace] runnung on [" + __dirname + "]");
-		_log("[trace] running with options " + JSON.stringify(options));
+		logger.info("### START ###");
+		logger.info("runnung on [" + __dirname + "]");
+		logger.info("running with options " + JSON.stringify(options));
 
 		// アプリケーションの初期化
 		require("ejs");
@@ -119,9 +240,9 @@ function application() {
 		var server = http.listen(options["port"], this._handler_on_ready);
 
 		if (server.address() != null)
-			_log("Node.js is listening to port: " + server.address().port);
+			logger.info("Node.js is listening to port: " + server.address().port);
 		else
-			_log("Node.js is listening to port: (unknown)");
+			logger.info("Node.js is listening to port: (unknown)");
 	}.bind(this);
 }
 
@@ -144,17 +265,16 @@ function peer_connection(owner, io, socket) {
 	this._socket = socket;
 
 	this._on_disconnect = function(m) {
-
-		_log("[TRACE] Bye... >> ((( ﾟ_ ﾟ)");
+		logger.trace("Bye... >> ((( ﾟ_ ﾟ)");
 		this._owner._sessions--;
-		_log("(" + this._owner._sessions + ") connection(s) alive.");
+		logger.info("(" + this._owner._sessions + ") connection(s) alive.");
 		this._io.emit("sessions changed", {count: this._owner._sessions});
 	}.bind(this);
 
 	this._on_chat_message = function(m) {
 		const uuid4 = require("uuid/v4");
-		_log("[TRACE] caught message: {x:" + m.x +", y:" + m.y + ", text:" + m.text + "} << (PEER)");
-		_log("[TRACE] BROADCAST! >> (EVERYONE)");
+		logger.trace("caught message: {x:" + m.x +", y:" + m.y + ", text:" + m.text + "} << (PEER)");
+		logger.trace("BROADCAST! >> (EVERYONE)");
 		m.id = uuid4();
 		m.background_color = util.generate_new_color();
 		_save_box(m);
@@ -176,55 +296,6 @@ function peer_connection(owner, io, socket) {
 ///////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////
-
-function _save_box(m) {
-
-	try {
-		var tree_object = _load_json_file("/tmp/.peta2-boxes-data.json");
-		require("json");
-		if (tree_object["data"] == null) {
-			tree_object["data"] = [];
-		}
-		boxes_array = tree_object["data"];
-		boxes_array.push(m);
-		var fs = require("fs");
-		var json_data = JSON.stringify(tree_object, null, "\t");
-		fs.writeFileSync("/tmp/.peta2-boxes-data.json", json_data, {encoding: "utf-8"});
-		// var json = ("/tmp/.peta2-boxes-data.json");
-		// res.json(json);
-	}
-	catch (err) {
-		_log(err);
-		_log("[error] json データの保存に失敗しています。");
-	}
-}
-
-function _log(message) {
-
-	try {
-		require("json");
-		var fs = require("fs");
-		fs.writeFileSync("/tmp/peta2.log", message + "\n", {encoding: "utf-8"});
-	}
-	catch (ex) {
-		console.log(ex);
-		console.log("[ERROR] ログ出力に失敗しています。");
-	}
-}
-
-function _load_json_file(path) {
-
-	try {
-		require("json");
-		var fs = require("fs");
-		return JSON.parse(fs.readFileSync(path, "utf-8"));
-	}
-	catch (ex) {
-		_log(ex);
-		_log("[info] ファイルが存在しません。");
-		return {}
-	}
-}
 
 function _read_commandline_arguments() {
 
